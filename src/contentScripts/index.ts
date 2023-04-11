d724228663bb536611fa2d4ef004f22b20815e28
YunYouJun <me@yunyoujun.cn>
2023-04-11 19:57:00 +0800

    chore: update info

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -10,7 +10,7 @@ import { setupApp } from '~/logic/common-setup'
 
   // communication example: send previous tab title from background page
   onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
+    console.log(`[dev-webext] Navigate from page "${data.title}"`)
   })
 
   // mount component to context window
